1a1f839d261857ab83374f7a2cfd0a62b8fc22a3
dousc <dousc123>
2024-09-23 16:47:06 +0800

    堆排序

---
+++ algorithm/src/main/java/com/example/algorithm/sort/HeapSort.java
@@ -0,0 +1,13 @@
+package com.example.algorithm.sort;
+
+public class HeapSort {
+    public static void heapSort(int[] arr) {
+        //第一次建立大根堆，从后往前依次调整
+        for (int i = arr.length/2 -1; i >= 0; i--) {
+            //从第一个非叶子结点从下至上，从右至左调整结构
+            //HeapAdjust(arr, i, arr.length);
+        }
+    }
+
+
+}
