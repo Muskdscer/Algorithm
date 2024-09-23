fde92b423ef978b2a2777b99e898d19c416f2ed5
dousc <dousc123>
2024-09-23 15:36:30 +0800

    冒泡排序

---
+++ algorithm/src/main/java/com/example/algorithm/sort/BubbleSort.java
@@ -0,0 +1,37 @@
+package com.example.algorithm.sort;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - j; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+    //优化
+    public static void bubbleSort2(int[] arr) {
+        int temp = 0;
+        boolean flag = false;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    flag = true;
+                }
+            }
+            //如果没有进入交换，说明已经排好序了
+            //如果进入了交换，把flag设置为false，判断下一次循环是否进入交换
+            if (!flag) {
+                break;
+            } else {
+                flag = false;
+            }
+        }
+    }
+}
