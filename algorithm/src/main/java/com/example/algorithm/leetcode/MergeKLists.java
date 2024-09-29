dcd53cf3f9d80aff0af8fe47e81eb3595e48318f
dousc <dousc123>
2024-09-29 16:07:26 +0800

    合并K个升序链接

---
+++ algorithm/src/main/java/com/example/algorithm/leetcode/MergeKLists.java
@@ -0,0 +1,32 @@
+package com.example.algorithm.leetcode;
+
+public class MergeKLists {
+    public ListNode mergeKLists(ListNode[] lists)
+    {
+        ListNode ans = null;
+        for (int i = 0; i < lists.length; i++) {
+            ans = mergeTwoLists(ans, lists[i]);
+        }
+        return ans;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 != null ? l1 : l2;
+        }
+        ListNode head = new ListNode(0);
+        ListNode cur = head , cur1 = l1, cur2 = l2;
+        while (cur1 != null && cur2 != null) {
+            if (cur1.val <= cur2.val) {
+                cur.next = cur1;
+                cur1 = cur1.next;
+            } else {
+                cur.next = cur2;
+                cur2 = cur2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = cur1 != null ? cur1 : cur2;
+        return head.next;
+    }
+}
