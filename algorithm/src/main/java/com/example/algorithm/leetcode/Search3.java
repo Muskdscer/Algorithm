085669110c282f88a46fcf7c37d7135298a1f703
DSC <D695312811@163.com>
2024-10-05 15:47:08 +0800

    leetcode高频--二分查找

---
+++ algorithm/src/main/java/com/example/algorithm/leetcode/Search3.java
@@ -0,0 +1,21 @@
+package com.example.algorithm.leetcode;
+
+public class Search3 {
+    public int search(int[] nums, int target) {
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid -1;
+            }
+        }
+        return -1;
+    }
+}
