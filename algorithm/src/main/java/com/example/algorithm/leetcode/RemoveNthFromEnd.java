b3c808cee8a4d662191d1354ac9ccfec1cd66327
DSC <D695312811@163.com>
2024-10-05 15:01:35 +0800

    leetcode高频--寻找两个正序数组的中位数

---
+++ algorithm/src/main/java/com/example/algorithm/leetcode/RemoveNthFromEnd.java
@@ -0,0 +1,23 @@
+package com.example.algorithm.leetcode;
+
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode fast = dummy;
+        ListNode slow = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast.next;
+        }
+
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+
+        slow.next = slow.next.next;
+        return dummy.next;
+    }
+}
