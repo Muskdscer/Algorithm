51b4a22673abcf9f7c801cac399fb60e5ae1a9e8
DSC <D695312811@163.com>
2024-10-07 10:49:14 +0800

    leetcode高频--二叉树右视图

---
+++ algorithm/src/main/java/com/example/algorithm/leetcode/RightSideView.java
@@ -0,0 +1,38 @@
+package com.example.algorithm.leetcode;
+
+
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+public class RightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        Deque<TreeNode> que = new LinkedList<>();
+
+        if (root == null) {
+            return list;
+        }
+        que.offerLast(root);
+        while (!que.isEmpty()) {
+            int levelSize = que.size();
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode poll = que.pollFirst();
+
+                if (poll.left != null) {
+                    que.addLast(poll.left);
+                }
+                if (poll.right != null) {
+                    que.addLast(poll.right);
+                }
+
+                if (i == levelSize - 1) {
+                    list.add(poll.val);
+                }
+            }
+        }
+        return list;
+    }
+}
