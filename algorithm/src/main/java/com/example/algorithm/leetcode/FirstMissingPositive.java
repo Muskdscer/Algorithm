041ad71e3f110adde0ddbca3a439105d61b3d0f4
Muskdscer <D695312811@163.com>
2024-10-08 11:52:49 +0800

    leetcode高频--41.缺失的第一个正数

---
+++ algorithm/src/main/java/com/example/algorithm/leetcode/FirstMissingPositive.java
@@ -0,0 +1,27 @@
+package com.example.algorithm.leetcode;
+
+/**
+ * 41. 缺失的第一个正数
+ */
+public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] <= 0) {
+                nums[i] = n + 1;
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            int num = Math.abs(nums[i]);
+            if (num <= n) {
+                nums[num - 1] = -Math.abs(nums[num - 1]);
+            }
+        }
+        for (int i = 0; i < n; ++i) {
+            if (nums[i] > 0) {
+                return i + 1;
+            }
+        }
+        return n + 1;
+    }
+}
