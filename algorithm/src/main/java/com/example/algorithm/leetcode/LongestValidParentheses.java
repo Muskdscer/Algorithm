3f2ce72e7309ae96828dc14057e98530aa319681
Muskdscer <D695312811@163.com>
2024-10-08 13:40:33 +0800

    leetcode高频--32.最长有效括号

---
+++ algorithm/src/main/java/com/example/algorithm/leetcode/LongestValidParentheses.java
@@ -0,0 +1,26 @@
+package com.example.algorithm.leetcode;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class LongestValidParentheses {
+    public int longestValidParentheses(String s) {
+        int maxans = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        stack.push(-1);
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+                if (stack.isEmpty()) {
+                    stack.push(i);
+                } else {
+                    maxans = Math.max(maxans, i - stack.peek());
+                }
+            }
+        }
+        return maxans;
+    }
+}
