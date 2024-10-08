9bcb97e39e5d25ad8aa43fa190b50ddf01733214
dousc <dousc123>
2024-10-08 10:15:51 +0800

    leetcode高频--69. x 的平方根

---
+++ algorithm/src/main/java/com/example/algorithm/leetcode/MySqrt.java
@@ -0,0 +1,19 @@
+package com.example.algorithm.leetcode;
+
+public class MySqrt {
+    public int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+        double C = x, x0 = x;
+        while (true) {
+            double xi = 0.5 * (x0 + C / x0);
+            if (Math.abs(x0 - xi) < 1e-7) {
+                break;
+            }
+            x0 = xi;
+        }
+        return (int) x0;
+    }
+}
