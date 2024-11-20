309e806649cd63eec46c673b07af0e888dbd56ba
DSC <D695312811@163.com>
2024-11-20 19:39:44 +0800

    二叉树

---
+++ algorithm/src/main/java/com/example/algorithm/kamaCoder/binaryTree/Recursion.java
@@ -0,0 +1,54 @@
+package com.example.algorithm.kamaCoder.binaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Recursion {
+    //前序
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preorder(root,result);
+        return result;
+    }
+
+    private void preorder(TreeNode root, List<Integer> result) {
+        if (root == null) {
+            return;
+        }
+        result.add(root.val);
+        preorder(root.left,result);
+        preorder(root.right,result);
+    }
+
+    //中序
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    void inorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, list);
+        list.add(root.val);             // 注意这一句
+        inorder(root.right, list);
+    }
+
+    //后续
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        postorder(root, res);
+        return res;
+    }
+
+    void postorder(TreeNode root, List<Integer> list) {
+        if (root == null) {
+            return;
+        }
+        postorder(root.left, list);
+        postorder(root.right, list);
+        list.add(root.val);             // 注意这一句
+    }
+}
