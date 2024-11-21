08f4b2340b94d005709a0cfc03748497ab489541
DSC <D695312811@163.com>
2024-11-21 17:07:34 +0800

    数量/是否平衡

---
+++ algorithm/src/main/java/com/example/algorithm/kamaCoder/binaryTree/GetDepth.java
@@ -0,0 +1,73 @@
+package com.example.algorithm.kamaCoder.binaryTree;
+
+public class GetDepth {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode() {}
+        public TreeNode(int val) {
+            this.val = val;
+        }
+        public TreeNode(TreeNode left, TreeNode right, int val) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //是否平衡
+    static boolean flag =true;
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(6);
+        TreeNode node6 = new TreeNode(7);
+        TreeNode node7 = new TreeNode(8);
+        TreeNode node8 = new TreeNode(9);
+        TreeNode node9 = new TreeNode(10);
+        TreeNode node10 = new TreeNode(11);
+
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.left = node5;
+        node2.right = node6;
+        node3.left = node7;
+        node3.right = node8;
+        node4.left = node9;
+        node4.right = node10;
+
+        flag = IsB(root);
+    }
+
+    private static boolean IsB(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private static int getHeight(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftHeight = getHeight(root.left);
+        if (leftHeight == -1) {
+            return -1;
+        }
+
+        int rightHeight = getHeight(root.left);
+        if (rightHeight == -1) {
+            return -1;
+        }
+
+        if (Math.abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+
+        return Math.max(leftHeight, rightHeight) + 1;
+
+    }
+}
---
+++ algorithm/src/main/java/com/example/algorithm/kamaCoder/binaryTree/GetNodesNum.java
@@ -0,0 +1,57 @@
+package com.example.algorithm.kamaCoder.binaryTree;
+
+public class GetNodesNum {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        public TreeNode() {}
+        public TreeNode(int val) {
+            this.val = val;
+        }
+        public TreeNode(TreeNode left, TreeNode right, int val) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    //数量
+    static int num = 0;
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node3 = new TreeNode(4);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(6);
+        TreeNode node6 = new TreeNode(7);
+        TreeNode node7 = new TreeNode(8);
+        TreeNode node8 = new TreeNode(9);
+        TreeNode node9 = new TreeNode(10);
+        TreeNode node10 = new TreeNode(11);
+
+        root.left = node1;
+        root.right = node2;
+        node1.left = node3;
+        node1.right = node4;
+        node2.left = node5;
+        node2.right = node6;
+        node3.left = node7;
+        node3.right = node8;
+        node4.left = node9;
+        node4.right = node10;
+
+        num = getNum(root);
+    }
+
+    private static int getNum(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = getNum(root.left);
+        int right = getNum(root.right);
+        return left + right + 1;
+    }
+}
