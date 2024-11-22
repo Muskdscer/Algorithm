a59f0b5b76fcbe3a312123d8897e6fed742b2372
DSC <D695312811@163.com>
2024-11-22 21:31:17 +0800

    右旋字符串

---
+++ algorithm/src/main/java/com/example/algorithm/kamaCoder/str/ReverseAllString.java
@@ -0,0 +1,32 @@
+package com.example.algorithm.kamaCoder.str;
+
+import java.lang.*;
+import java.util.*;
+import java.io.*;
+
+public class ReverseAllString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = Integer.parseInt(in.nextLine());
+        String s = in.nextLine();
+
+
+        int len = s.length();  //获取字符串长度
+        char[] chars = s.toCharArray();
+        reverseString(chars, 0, len - 1);  //反转整个字符串
+        reverseString(chars, 0, n - 1);  //反转前一段字符串，此时的字符串首尾尾是0,n - 1
+        reverseString(chars, n, len - 1);  //反转后一段字符串，此时的字符串首尾尾是n,len - 1
+        System.out.println(chars);
+    }
+
+    private static void reverseString(char[] ch, int start, int end) {
+        //异或法反转字符串，参照题目 344.反转字符串的解释
+        while (start < end) {
+            ch[start] ^= ch[end];
+            ch[end] ^= ch[start];
+            ch[start] ^= ch[end];
+            start++;
+            end--;
+        }
+    }
+}
