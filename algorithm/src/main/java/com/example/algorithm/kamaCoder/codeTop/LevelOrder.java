1d7231740a395c4d034cc408d51f9120cda9143f
Muskdscer <D695312811@163.com>
2024-11-26 16:01:40 +0800

    codeTop

---
+++ algorithm/src/main/java/com/example/algorithm/kamaCoder/codeTop/LevelOrder.java
@@ -0,0 +1,53 @@
+package com.example.algorithm.kamaCoder.codeTop;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    List<List<Integer>> res = new ArrayList<List<Integer>>();
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<Integer>();
+            int currentLevelSize = queue.size();
+            for (int i = 0; i < currentLevelSize - 1; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
---
+++ algorithm/src/main/java/com/example/algorithm/kamaCoder/codeTop/MergeTwoLists.java
@@ -0,0 +1,36 @@
+package com.example.algorithm.kamaCoder.codeTop;
+
+import com.example.algorithm.kamaCoder.TreeNode;
+
+public class MergeTwoLists {
+    static class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
+    public static void main(String[] args) {
+        ListNode head1 = new ListNode(1);
+        ListNode head2 = new ListNode(2);
+        ListNode head = merge(head1, head2);
+    }
+
+    public static ListNode merge(ListNode l1, ListNode l2) {
+        ListNode preHead = new ListNode(-1);
+        ListNode prev = preHead;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+        prev.next = l1 == null ? l2 : l1;
+        return preHead.next;
+    }
+}
