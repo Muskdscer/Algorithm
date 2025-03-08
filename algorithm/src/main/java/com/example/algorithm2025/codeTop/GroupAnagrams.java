52c36cc159ceea4f81710b30c33e5742a30987a4
Muskdscer <D695312811@163.com>
2025-03-08 14:47:45 +0800

    hot100刷题

---
+++ algorithm/src/main/java/com/example/algorithm2025/codeTop/GroupAnagrams.java
@@ -0,0 +1,45 @@
+package com.example.algorithm2025.codeTop;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public static void main(String[] args) {
+        String[] strs = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+        List<List<String>> result = groupAnagrams(strs);
+        System.out.println(result.size());
+        for (int i = 0; i < result.size(); i++) {
+            for (int j = 0; j < result.get(i).size(); j++) {
+                System.out.print(result.get(i).get(j) + " ");
+            }
+        }
+    }
+
+    private static List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) {
+            return new ArrayList<>();
+        }
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] count = new int[26];
+            for (int i = 0; i < str.length(); i++) {
+                count[str.charAt(i) - 'a']++;
+            }
+
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < 26; i++) {
+                if (count[i] != 0) {
+                    sb.append((char) ('a' + i));
+                    sb.append(count[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> strings = map.getOrDefault(key, new ArrayList<String>());
+            strings.add(str);
+            map.put(key, strings);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/TwoSum.java
@@ -0,0 +1,24 @@
+package com.example.algorithm2025.leetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result =  twoSum(nums, target);
+        System.out.println("目标值为:" + result[0] + "  " + result[1]);
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+}
