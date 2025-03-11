1376602c4b8326722f01073c57e0e660b9427e2e
Muskdscer <D695312811@163.com>
2025-03-11 15:43:52 +0800

    hot100刷题-相交链表

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/linkedlist/GetIntersectionNode.java
@@ -0,0 +1,17 @@
+package com.example.algorithm2025.leetCode.linkedlist;
+
+public class GetIntersectionNode {
+    public static void main(String[] args) {
+    }
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA,  pB = headB;
+        while (pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        return pA;
+    }
+}
