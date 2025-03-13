23a752b4edb1f53939b2809bde1033a9b80a4fda
Muskdscer <D695312811@163.com>
2025-03-13 10:20:21 +0800

    hot100刷题-相交链表

--- algorithm/src/main/java/com/example/algorithm2025/leetCode/linkedlist/GetIntersectionNode.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/linkedlist/GetIntersectionNode.java
@@ -2,6 +2,11 @@ package com.example.algorithm2025.leetCode.linkedlist;
 
 public class GetIntersectionNode {
     public static void main(String[] args) {
+        System.out.println('a' + 'b');
+        System.out.println('d' - 'a');
+        System.out.println('a' + 'A');
+        System.out.println('a' + 3);
+        System.out.println('A');
     }
     public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) {
