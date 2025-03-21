0f60708ec2febab946626d75014344d3a018cf28
Muskdscer <D695312811@163.com>
2025-03-21 16:04:05 +0800

    hot100刷题-二叉树 中序遍历

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/binaryTree/InorderTraversal.java
@@ -0,0 +1,21 @@
+package com.example.algorithm2025.leetCode.binaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        inorder(root, res);
+        return res;
+    }
+
+    public void inorder(TreeNode root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, res);
+        res.add(root.val);
+        inorder(root.right, res);
+    }
+}
