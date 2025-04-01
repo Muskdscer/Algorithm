4a7b48022b748527fb0ba13aa41000968f4378fe
Muskdscer <D695312811@163.com>
2025-04-01 14:00:34 +0800

    hot100刷题-缺失的第一个正数

--- algorithm/src/main/java/com/example/algorithm2025/leetCode/array/FirstMissingPositive.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/array/FirstMissingPositive.java
@@ -8,6 +8,26 @@ public class FirstMissingPositive {
     }
 
     private static int firstMissingPositive(int[] nums) {
-        return 0;
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            while (nums[i] > 0 && nums[i] <= len && nums[nums[i] - 1] != nums[i]) {
+                // 满足在指定范围内、并且没有放在正确的位置上，才交换
+                // 例如：数值 3 应该放在索引 2 的位置上
+                swap(nums, nums[i] - 1, i);
+            }
+        }
+        // [1, -1, 3, 4]
+        for (int i = 0; i < len; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+        //都正确的返回数组长度 + 1
+        return len + 1;
+    }
+    private static void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
     }
 }
