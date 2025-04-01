9e8e1382f4d3e725bdf9ca6dc2b55e19b84e5dda
DSC <D695312811@163.com>
2025-04-01 21:18:05 +0800

    leetcode-hot100 二叉树最大深度

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/binaryTree/MaxDepth.java
@@ -0,0 +1,15 @@
+package com.example.algorithm2025.leetCode.binaryTree;
+
+public class MaxDepth {
+    /**
+     * 递归法
+     */
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        return Math.max(leftDepth, rightDepth) + 1;
+    }
+}
