ca70ef4cef4392c5a2d62fafbb486e0e0c747de0
Muskdscer <D695312811@163.com>
2025-04-01 16:45:27 +0800

    hot100刷题-旋转矩阵

--- algorithm/src/main/java/com/example/algorithm2025/leetCode/matrix/SpiralOrder.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/matrix/SpiralOrder.java
@@ -1,5 +1,6 @@
 package com.example.algorithm2025.leetCode.matrix;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SpiralOrder {
@@ -12,6 +13,32 @@ public class SpiralOrder {
     }
 
     private static List<Integer> spiralOrder(int[][] matrix) {
-        return null;
+        List<Integer> order = new ArrayList<Integer>();
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return order;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        int left = 0, right = columns - 1, top = 0, bottom = rows - 1;
+        while (left <= right && top <= bottom) {
+            for (int column = left; column <= right; column++) {
+                order.add(matrix[top][column]);
+            }
+            for (int row = top + 1; row <= bottom; row++) {
+                order.add(matrix[row][right]);
+            }
+            if (left < right && top < bottom) {
+                for (int column = right - 1; column > left; column--) {
+                    order.add(matrix[bottom][column]);
+                }
+                for (int row = bottom; row > top; row--) {
+                    order.add(matrix[row][left]);
+                }
+            }
+            left++;
+            right--;
+            top++;
+            bottom--;
+        }
+        return order;
     }
 }
