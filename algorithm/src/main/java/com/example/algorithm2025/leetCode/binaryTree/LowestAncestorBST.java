1d89103c589dde6722b3c1694651a0b0ab23917d
Muskdscer <D695312811@163.com>
2025-04-14 16:02:06 +0800

    hot100刷题-二叉树最近公共祖先

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/binaryTree/LowestAncestorBST.java
@@ -0,0 +1,13 @@
+package com.example.algorithm2025.leetCode.binaryTree;
+
+public class LowestAncestorBST {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root.val > p.val && root.val > q.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+        if (root.val < p.val && root.val < q.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+        return root;
+    }
+}
