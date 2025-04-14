45395fbbb7968e70d8bc904c46434181cabb7c58
Muskdscer <D695312811@163.com>
2025-04-14 17:37:28 +0800

    hot100刷题-二叉树最大路径和

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/binaryTree/MaxPathSum1.java
@@ -0,0 +1,20 @@
+package com.example.algorithm2025.leetCode.binaryTree;
+
+public class MaxPathSum1 {
+    int max = Integer.MIN_VALUE;
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return max;
+    }
+
+    private int maxGain(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = Math.max(maxGain(root.left), 0);
+        int right = Math.max(maxGain(root.right), 0);
+        int priceNewpath = root.val + left + right;
+        max = Math.max(priceNewpath, max);
+        return Math.max(left, right) + root.val;
+    }
+}
