0f37a0b5e91eb5bb7e2a84f900de97abcc0e288d
Muskdscer <D695312811@163.com>
2025-04-15 16:07:35 +0800

    hot100刷题-回溯 全排列

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/backTrack/Permute.java
@@ -0,0 +1,37 @@
+package com.example.algorithm2025.leetCode.backTrack;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Permute {
+    List<List<Integer>> result = new ArrayList<>();//收集结果
+    LinkedList<Integer> path = new LinkedList<>();//存放符合条件的结果
+    boolean[] used;
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums.length == 0) {
+            return result;
+        }
+        used = new boolean[nums.length];
+        permuteHelper(nums);
+        return result;
+    }
+
+    public void permuteHelper(int[] nums) {
+        if (path.size() == nums.length) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (used[i]) {
+                continue;
+            }
+            used[i] = true;
+            path.add(nums[i]);
+            permuteHelper(nums);
+            path.removeLast();
+            used[i] = false;
+        }
+    }
+}
