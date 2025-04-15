33ca20e24966fb97a61968f7ef978bb74d14afc5
Muskdscer <D695312811@163.com>
2025-04-15 16:24:31 +0800

    hot100刷题-二分查找 搜索插入位置

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/binarySearch/Search.java
@@ -0,0 +1,25 @@
+package com.example.algorithm2025.leetCode.binarySearch;
+
+public class Search {
+    public int search(int[] nums, int target) {
+        // 避免当 target 小于nums[0] nums[nums.length - 1]时多次循环运算
+        if (target < nums[0] || target > nums[nums.length - 1]) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            }
+            else { // nums[mid] > target
+                right = mid - 1;
+            }
+        }
+        // 未找到目标值
+        return -1;
+    }
+}
