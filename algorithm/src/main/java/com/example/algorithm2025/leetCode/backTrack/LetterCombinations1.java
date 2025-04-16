b4fc91bb9a6c39b238d94922bab24a6a7841afa2
DSC <D695312811@163.com>
2025-04-16 20:37:23 +0800

    leetcode-回溯 电话号码字母组合

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/backTrack/LetterCombinations1.java
@@ -0,0 +1,30 @@
+package com.example.algorithm2025.leetCode.backTrack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LetterCombinations1 {
+    String[] numString = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+    List<String> list = new ArrayList<>();
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length() == 0) {
+            return list;
+        }
+        combineHelper(digits, 0);
+        return list;
+    }
+
+    StringBuilder sb = new StringBuilder();
+    private void combineHelper(String digits, int num) {
+        if (num == digits.length()) {
+            list.add(sb.toString());
+            return;
+        }
+        String str = numString[digits.charAt(num) - '0'];
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(str.charAt(i));
+            combineHelper(digits, num + 1);
+            sb.deleteCharAt(sb.length() - 1);
+        }
+    }
+}
