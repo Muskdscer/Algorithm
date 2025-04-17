1cb8c9eb9c3bee7836c4b0ba10bb070b06223912
DSC <D695312811@163.com>
2025-04-17 20:45:23 +0800

    leetcode-回溯 搜索二维矩阵

--- algorithm/src/main/java/com/example/algorithm2025/leetCode/binarySearch/SearchMatrix.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/binarySearch/SearchMatrix.java
@@ -2,6 +2,23 @@ package com.example.algorithm2025.leetCode.binarySearch;
 
 public class SearchMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
-        return true;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        //左闭右闭
+        int left = 0;
+        int right = m * n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int x = matrix[mid / n][mid % n];
+            if (x == target) {
+                return true;
+            }
+            if (x < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
     }
 }
