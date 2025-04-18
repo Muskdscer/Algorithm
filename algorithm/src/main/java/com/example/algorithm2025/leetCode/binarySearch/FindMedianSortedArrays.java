7a81fe263dc7f1565c4524bb358b16a8a554e4cb
Muskdscer <D695312811@163.com>
2025-04-18 09:18:52 +0800

    hot100刷题-二分法 寻找两个正序数组中位数

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/binarySearch/FindMedianSortedArrays.java
@@ -0,0 +1,38 @@
+package com.example.algorithm2025.leetCode.binarySearch;
+
+public class FindMedianSortedArrays {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int iMin = 0, iMax = m;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = (m + n + 1) / 2 - i;
+            if (j != 0 && i != m && nums2[j-1] > nums1[i]){ // i 需要增大
+                iMin = i + 1;
+            }
+            else if (i != 0 && j != n && nums1[i-1] > nums2[j]) { // i 需要减小
+                iMax = i - 1;
+            }
+            else { // 达到要求，并且将边界条件列出来单独考虑
+                int maxLeft = 0;
+                if (i == 0) { maxLeft = nums2[j-1]; }
+                else if (j == 0) { maxLeft = nums1[i-1]; }
+                else { maxLeft = Math.max(nums1[i-1], nums2[j-1]); }
+                if ( (m + n) % 2 == 1 ) { return maxLeft; } // 奇数的话不需要考虑右半部分
+
+                int minRight = 0;
+                if (i == m) { minRight = nums2[j]; }
+                else if (j == n) { minRight = nums1[i]; }
+                else { minRight = Math.min(nums2[j], nums1[i]); }
+
+                return (maxLeft + minRight) / 2.0; //如果是偶数的话返回结果
+            }
+        }
+        return 0.0;
+    }
+}
