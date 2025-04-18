47cf17150ea6651dcc38f3a6eb1e153f40a10d8c
Muskdscer <D695312811@163.com>
2025-04-18 11:07:31 +0800

    hot100刷题-堆 前K个高频元素

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/heap/TopKFrequent.java
@@ -0,0 +1,27 @@
+package com.example.algorithm2025.leetCode.heap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class TopKFrequent {
+    //基于大顶堆实现
+    public int[] topKFrequent1(int[] nums, int k) {
+        Map<Integer,Integer> map = new HashMap<>(); //key为数组元素值,val为对应出现次数
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num,0) + 1);
+        }
+        //在优先队列中存储二元组(num, cnt),cnt表示元素值num在数组中的出现次数
+        //出现次数按从队头到队尾的顺序是从大到小排,出现次数最多的在队头(相当于大顶堆)
+        PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair2[1] - pair1[1]);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.add(new int[]{entry.getKey(), entry.getValue()});
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            //依次从队头弹出k个,就是出现频率前k高的元素
+            ans[i] = pq.poll()[0];
+        }
+        return ans;
+    }
+}
