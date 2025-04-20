136aa12cc60847dc5e04f41b64b01a574c9ee54b
DSC <D695312811@163.com>
2025-04-20 15:56:12 +0800

    leetcode-动规 乘积最大子数组

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/dp/MaxProduct.java
@@ -0,0 +1,19 @@
+package com.example.algorithm2025.leetCode.dp;
+
+public class MaxProduct {
+    public int maxProduct(int[] nums) {
+        int max = Integer.MIN_VALUE, imax = 1, imin = 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] < 0) {
+                int temp = imax;
+                imax = imin;
+                imin = temp;
+            }
+            imax = Math.max(imax * nums[i], nums[i]);
+            imin = Math.min(imin * nums[i], nums[i]);
+
+            max = Math.max(imax, max);
+        }
+        return max;
+    }
+}
