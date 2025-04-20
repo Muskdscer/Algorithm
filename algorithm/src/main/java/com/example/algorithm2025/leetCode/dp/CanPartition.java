1513d4bbf4e18b7c2c10ea7862017f961597ca42
DSC <D695312811@163.com>
2025-04-20 16:31:18 +0800

    leetcode-动规 分割等和子集

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/dp/CanPartition.java
@@ -0,0 +1,29 @@
+package com.example.algorithm2025.leetCode.dp;
+
+public class CanPartition {
+    public boolean canPartition(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return false;
+        }
+        int n = nums.length;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        int target = sum / 2;
+        int[] dp = new int[target + 1];
+        for (int i = 0; i < n; i++) {
+            for (int j = target; j >= nums[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - nums[i]] + nums[i]);
+            }
+            //剪枝一下
+            if (dp[target] == target) {
+                return true;
+            }
+        }
+        return dp[target] == target;
+    }
+}
