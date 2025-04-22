b8d9086e9bfc39d0cab2185334a1543f5fdbfc0a
Muskdscer <D695312811@163.com>
2025-04-22 14:34:20 +0800

    hot100刷题-排序 快排

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/sort/QuickSort.java
@@ -0,0 +1,27 @@
+package com.example.algorithm2025.leetCode.sort;
+
+public class QuickSort {
+    public void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivot = partition(arr, left, right);
+            quickSort(arr, left, pivot - 1);
+            quickSort(arr, pivot + 1, right);
+        }
+    }
+
+    public int partition(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        while (left < right) {
+            while (left < right && arr[right] >= pivot) {
+                right--;
+            }
+            arr[left] = arr[right];
+            while (left < right && arr[left] <= pivot) {
+                left++;
+            }
+            arr[right] = arr[left];
+        }
+        arr[left] = pivot;
+        return left;
+    }
+}
