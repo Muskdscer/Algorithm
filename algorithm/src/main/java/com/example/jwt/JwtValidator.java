769fce7b352ea9407e86343abda6cf78f6e6a03c
Muskdscer <D695312811@163.com>
2025-06-09 16:04:41 +0800

    jwt学习

---
+++ algorithm/src/main/java/com/example/jwt/JwtValidator.java
@@ -0,0 +1,10 @@
+package com.example.jwt;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JwtValidator {
+    static List<String> getRoles(String jwtToken) {
+        return new ArrayList<>();
+    }
+}
