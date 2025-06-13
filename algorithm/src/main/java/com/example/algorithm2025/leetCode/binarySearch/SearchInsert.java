1b12c017e0aaa886b69bb1a90baf6c5fb5c32dfb
Muskdscer <D695312811@163.com>
2025-06-13 21:27:39 +0800

    hot100刷题-搜索插入位置

--- algorithm/src/main/java/com/example/algorithm2025/leetCode/binarySearch/SearchInsert.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetCode/binarySearch/SearchInsert.java
@@ -9,16 +9,16 @@ public class SearchInsert {
      */
     public int search(int[] nums, int target) {
         int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {
+            } else if(nums[mid] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
-        return -1;
+        return left;
     }
 }
--- algorithm/src/main/java/com/example/algorithm2025/leetcode01/binarySearch/SearchInsert.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binarySearch/SearchInsert.java
@@ -1,4 +1,19 @@
 package com.example.algorithm2025.leetcode01.binarySearch;
 
 public class SearchInsert {
+    //左闭右闭
+    public int searchInsert(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while(left <= right) {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
 }
