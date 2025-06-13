2d67940ba7224afadbe4728522910dfbbb18a431
Muskdscer <D695312811@163.com>
2025-06-13 10:49:33 +0800

    hot100刷题-最大子数组和

--- algorithm/src/main/java/com/example/algorithm2025/leetcode01/array/MaxSubArray.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/array/MaxSubArray.java
@@ -1,4 +1,27 @@
 package com.example.algorithm2025.leetcode01.array;
 
 public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int result = maxSubArray(nums);
+        System.out.println("最大子数组和为： " + result);
+    }
+    public static int maxSubArray(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return max;
+    }
 }
