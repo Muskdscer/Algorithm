447d5ef08712e607acab23d4327b4abe3aee20c8
Muskdscer <D695312811@163.com>
2025-06-13 17:05:32 +0800

    hot100刷题-矩阵置零

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/matrix/SetZeroes1.java
@@ -0,0 +1,35 @@
+package com.example.algorithm2025.leetcode01.matrix;
+
+public class SetZeroes1 {
+    public static void main(String[] args) {
+        int[][] matrix = new int[][]{{1, 1, 1}, {1, 0, 1}, {1, 1, 1}};
+        setZeroes(matrix);
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                System.out.println("置零后结果： " + matrix[i][j]);
+            }
+        }
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        boolean[] row = new boolean[m];
+        boolean[] col = new boolean[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = col[j] = true;
+                }
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (row[i] || col[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+}
