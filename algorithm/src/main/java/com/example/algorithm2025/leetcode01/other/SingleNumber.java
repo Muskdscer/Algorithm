16d540c1380c42229ed4024b0acad4fca48de4fe
Muskdscer <D695312811@163.com>
2025-06-13 21:33:06 +0800

    hot100刷题-只出现一次数字

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/other/SingleNumber.java
@@ -0,0 +1,11 @@
+package com.example.algorithm2025.leetcode01.other;
+
+public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
+}
