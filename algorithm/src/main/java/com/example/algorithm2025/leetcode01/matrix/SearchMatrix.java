125adf7b34943f7cf40ac083281c5f8ff38e841f
Muskdscer <D695312811@163.com>
2025-06-13 17:30:57 +0800

    hot100刷题-搜索二维矩阵II

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/matrix/SearchMatrix.java
@@ -0,0 +1,17 @@
+package com.example.algorithm2025.leetcode01.matrix;
+
+public class SearchMatrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = matrix.length - 1, j = 0;
+        while (i >= 0 && j < matrix[0].length) {
+            if (matrix[i][j] > target) {
+                i--;
+            } else if (matrix[i][j] < target) {
+                j++;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
