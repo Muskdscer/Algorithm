f31ef7c7a3162397532e3f872a5ec1c00c2c32b5
DSC <D695312811@163.com>
2025-06-14 09:47:39 +0800

    hot100刷题-在排序数组中查找元素的第一个和最后一个位置

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binarySearch/SearchRange.java
@@ -0,0 +1,38 @@
+package com.example.algorithm2025.leetcode01.binarySearch;
+
+public class SearchRange {
+    public int[] searchRange(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length- 1;
+        int first = -1;
+        int last = -1;
+        //找到第一个等于target的位置
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target){
+                first = mid;
+                right = mid - 1;//重点
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        //最后一个等于target的位置
+        left = 0;
+        right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                last = mid;
+                left = mid + 1;//重点
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return new int[]{first, last};
+    }
+}
