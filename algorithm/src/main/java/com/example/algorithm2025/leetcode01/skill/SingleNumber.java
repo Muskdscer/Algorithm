43047dc7d120cbfbd19ce7ebb3f4bd59f922db3c
Muskdscer <D695312811@163.com>
2025-06-15 15:09:50 +0800

    hot100刷题-只出现一次数字、多数元素

--- algorithm/src/main/java/com/example/algorithm2025/leetcode01/skill/SingleNumber.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/skill/SingleNumber.java
@@ -1,4 +1,11 @@
 package com.example.algorithm2025.leetcode01.skill;
 
 public class SingleNumber {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int num : nums) {
+            ans ^= num;
+        }
+        return ans;
+    }
 }
