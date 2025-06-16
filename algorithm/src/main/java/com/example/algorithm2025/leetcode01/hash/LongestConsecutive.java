f9671932406f34a1f1c02bbeb205f9b2f9867790
Muskdscer <D695312811@163.com>
2025-06-16 10:47:39 +0800

    hot100刷题-最长连续序列

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/hash/LongestConsecutive.java
@@ -0,0 +1,26 @@
+package com.example.algorithm2025.leetcode01.hash;
+
+import java.util.HashSet;
+
+public class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+        for (int num : nums) {
+            numSet.add(num);
+        }
+
+        int longestStreak = 0;
+        for (Integer num : numSet) {
+            if (!numSet.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+                while (numSet.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+        return longestStreak;
+    }
+}
