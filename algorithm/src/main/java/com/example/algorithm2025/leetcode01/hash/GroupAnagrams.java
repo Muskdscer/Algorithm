d8ba5dd215e71711aa28887360ad2930b712f33f
Muskdscer <D695312811@163.com>
2025-06-16 10:38:56 +0800

    hot100刷题-字母异位词分组

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/hash/GroupAnagrams.java
@@ -0,0 +1,35 @@
+package com.example.algorithm2025.leetcode01.hash;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GroupAnagrams {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs.length == 0) {
+            return new ArrayList<>();
+        }
+
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            int[] count = new int[26];
+            for (int i = 0; i < str.length(); i++) {
+                count[str.charAt(i) - 'a']++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 26; i++) {
+                if (count[i] != 0) {
+                    sb.append((char) 'a' + i);
+                    sb.append(count[i]);
+                }
+            }
+            String key = sb.toString();
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
