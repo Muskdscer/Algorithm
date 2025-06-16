0bd13d872edfd9c5f554b109c79f00584751cb93
Muskdscer <D695312811@163.com>
2025-06-16 13:29:37 +0800

    hot100刷题-颜色分类

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/skill/SortColors.java
@@ -0,0 +1,18 @@
+package com.example.algorithm2025.leetcode01.skill;
+
+public class SortColors {
+    public void sortColors(int[] nums) {
+        int p0 = 0;
+        int p1 = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int x = nums[i];
+            nums[i] = 2;
+            if (x <= 1) {
+                nums[p1++] = 1;
+            }
+            if (x == 0) {
+                nums[p0++] = 0;
+            }
+        }
+    }
+}
