15cc1d4419c56487d86a7935ddb5f68c3ce2c5cf
Muskdscer <D695312811@163.com>
2025-07-01 16:05:02 +0800

    hot100刷题-二叉树最大深度

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/MaxDepth.java
@@ -0,0 +1,12 @@
+package com.example.algorithm2025.leetcode01.binaryTree;
+
+public class MaxDepth {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+    }
+}
