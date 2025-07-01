3521d47668cb63f38572650874e760fc58e2ca6e
Muskdscer <D695312811@163.com>
2025-07-01 15:52:31 +0800

    hut100刷题-中序遍历

--- algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/InorderTraversal.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/InorderTraversal.java
@@ -1,4 +1,24 @@
 package com.example.algorithm2025.leetcode01.binaryTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class InorderTraversal {
+    List<Integer> res = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        inorder(root);
+        return res;
+    }
+
+    private void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        res.add(root.val);
+        inorder(root.right);
+    }
 }
