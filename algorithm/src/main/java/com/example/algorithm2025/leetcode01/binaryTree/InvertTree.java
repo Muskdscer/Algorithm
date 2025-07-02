4cc991b18f062f25dc3cb39ece04749b6706377d
Muskdscer <D695312811@163.com>
2025-07-02 20:30:27 +0800

    hot100刷题-翻转二叉树

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/InvertTree.java
@@ -0,0 +1,19 @@
+package com.example.algorithm2025.leetcode01.binaryTree;
+
+public class InvertTree {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        invertTree(root.left);
+        invertTree(root.right);
+        swap(root,root.left, root.right);
+        return root;
+    }
+
+    private void swap(TreeNode root, TreeNode left, TreeNode right) {
+        TreeNode temp = left;
+        root.left = right;
+        root.right = temp;
+    }
+}
