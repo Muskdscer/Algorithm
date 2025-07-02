a100c1b01b6efd29558500054454131f9ef23227
Muskdscer <D695312811@163.com>
2025-07-02 15:21:40 +0800

    hot100刷题-有效的括号

--- algorithm/src/main/java/com/example/algorithm2025/leetcode01/stack/IsValid.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/stack/IsValid.java
@@ -1,4 +1,25 @@
 package com.example.algorithm2025.leetcode01.stack;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class IsValid {
+    public boolean isValid(String s) {
+        Deque<Character> que = new LinkedList<>();
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '[') {
+                que.push(']');
+            } else if (chars[i] == '{') {
+                que.push('}');
+            } else if (chars[i] == '(') {
+                que.push(')');
+            } else if (que.isEmpty() || que.peek() != chars[i]) {
+                return false;
+            } else {
+                que.pop();
+            }
+        }
+        return que.isEmpty();
+    }
 }
