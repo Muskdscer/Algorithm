3e3e5a22c55a5a0e1469735d03a77351ad77e99d
Muskdscer <D695312811@163.com>
2025-07-04 11:38:16 +0800

    hot100刷题-二叉树验证二叉搜索树

--- algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/IsValidBST.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/IsValidBST.java
@@ -1,24 +1,26 @@
 package com.example.algorithm2025.leetcode01.binaryTree;
 
 public class IsValidBST {
-    //递归
     TreeNode max;
     public boolean isValidBST(TreeNode root) {
         if (root == null) {
             return true;
         }
+
         //左
-        boolean left = isValidBST(root.left);
-        if (!left) {
+        boolean leftFlag = isValidBST(root.left);
+        if (!leftFlag) {
             return false;
         }
+
         //中
         if (max != null && max.val >= root.val) {
             return false;
         }
         max = root;
+
         //右
-        boolean right = isValidBST(root.right);
-        return right;
+        boolean rightFlag = isValidBST(root.right);
+        return rightFlag;
     }
 }
