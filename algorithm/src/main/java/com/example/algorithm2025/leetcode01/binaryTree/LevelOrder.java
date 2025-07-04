b67ed67397e3a12bbf7a0ab1afcd36d1ce841379
Muskdscer <D695312811@163.com>
2025-07-04 09:51:56 +0800

    hot100刷题-二叉树层序遍历

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/LevelOrder.java
@@ -0,0 +1,35 @@
+package com.example.algorithm2025.leetcode01.binaryTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class LevelOrder {
+    List<List<Integer>> res = new ArrayList<>();
+    Queue<TreeNode> que = new LinkedList<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+
+        que.offer(root);
+        while (!que.isEmpty()) {
+            int size = que.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = que.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    que.offer(node.left);
+                }
+                if (node.right != null) {
+                    que.offer(node.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
