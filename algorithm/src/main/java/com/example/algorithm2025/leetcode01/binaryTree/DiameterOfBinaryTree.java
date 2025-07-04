ce64fe5b4cc5b084392ed12840f71226b43d6a3c
Muskdscer <D695312811@163.com>
2025-07-04 09:15:28 +0800

    hot100刷题-二叉树直径

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/DiameterOfBinaryTree.java
@@ -0,0 +1,19 @@
+package com.example.algorithm2025.leetcode01.binaryTree;
+
+public class DiameterOfBinaryTree {
+    private int diameter = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        dfs(root);
+        return diameter;
+    }
+
+    private int dfs(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftLen = dfs(root.left);
+        int rightLen = dfs(root.right);
+        diameter = Math.max(diameter, leftLen + rightLen);
+        return Math.max(leftLen, rightLen) + 1;
+    }
+}
