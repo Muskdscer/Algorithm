c4e98d014081a0f6a0850849a8fc524e5ad9fe49
Muskdscer <D695312811@163.com>
2025-07-14 13:52:53 +0800

    hot100刷题-链表回文链表

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/linkedlist/IsPalindrome.java
@@ -0,0 +1,25 @@
+package com.example.algorithm2025.leetcode01.linkedlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsPalindrome {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+
+        int start = 0;
+        int end = list.size() - 1;
+        while (start < end) {
+            if (list.get(start) != list.get(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
