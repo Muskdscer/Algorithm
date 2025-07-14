c0d7d1414cf93dff7b34301923c37c104c04592e
Muskdscer <D695312811@163.com>
2025-07-14 15:10:28 +0800

    hot100刷题-链表删除倒数第N个结点

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/linkedlist/RemoveNthFromEnd.java
@@ -0,0 +1,22 @@
+package com.example.algorithm2025.leetcode01.linkedlist;
+
+public class RemoveNthFromEnd {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        while (n > 0) {
+            fast = fast.next;
+            n--;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        if (slow.next != null) {
+            slow.next = slow.next.next;
+        }
+        return dummy.next;
+    }
+}
