dcd046b2a9fa0c8142a553ce1accbd4c45e4f785
Muskdscer <D695312811@163.com>
2025-07-15 14:42:48 +0800

    hot100刷题-组合总和

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/backTrack/CombinationSum.java
@@ -0,0 +1,30 @@
+package com.example.algorithm2025.leetcode01.backTrack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CombinationSum {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);//先进行排序
+        backTracking(res, new ArrayList<>(), candidates, target, 0, 0);
+        return res;
+    }
+
+    private void backTracking(List<List<Integer>> res, List<Integer> path, int[] candidates, int target, int sum, int idx) {
+        // 找到了数字和为 target 的组合
+        if (sum == target) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = idx; i < candidates.length; i++) {
+            // 如果 sum + candidates[i] > target 就终止遍历
+            if (sum + candidates[i] > target) break;
+            path.add(candidates[i]);
+            backTracking(res, path, candidates, target, sum + candidates[i], i);
+            path.remove(path.size() - 1); // 回溯，移除路径 path 最后一个元素
+        }
+    }
+}
