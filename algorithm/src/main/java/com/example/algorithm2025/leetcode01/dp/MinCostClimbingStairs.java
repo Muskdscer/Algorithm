6633fe75f0ddf7a461d91ba320fc9a2c833cad9e
Muskdscer <D695312811@163.com>
2025-08-11 12:57:28 +0800

    斐波那契数列和最小花费爬楼梯

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/dp/MinCostClimbingStairs.java
@@ -0,0 +1,15 @@
+package com.example.algorithm2025.leetcode01.dp;
+
+//第一步不支付费用
+public class MinCostClimbingStairs {
+    public int climbStairs(int[] cost) {
+        int len = cost.length;
+        int[] dp = new int[len + 1];
+        dp[0] = 0;
+        dp[1] = 0;
+        for (int i = 2; i <= len; i++) {
+            dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+        }
+        return dp[len];
+    }
+}
