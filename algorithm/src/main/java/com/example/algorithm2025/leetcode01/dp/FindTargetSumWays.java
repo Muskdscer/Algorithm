711398dc951beec106877ef47d15d9b09ad1d153
Muskdscer <D695312811@163.com>
2025-08-11 15:58:55 +0800

    动态规划 目标和

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/dp/FindTargetSumWays.java
@@ -0,0 +1,26 @@
+package com.example.algorithm2025.leetcode01.dp;
+
+public class FindTargetSumWays {
+    public int findTargetSumWays(int[] nums, int target) {
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        if (Math.abs(target) > sum) {
+            return 0;
+        }
+        if ((target + sum) % 2 == 1) {
+            return 0;
+        }
+
+        int bagSize = (target + sum) / 2;
+        int[] dp = new int[bagSize + 1];
+        dp[0] = 1;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = bagSize; j >= nums[i]; j--) {
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return dp[bagSize];
+    }
+}
