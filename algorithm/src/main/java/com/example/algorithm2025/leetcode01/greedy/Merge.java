9177a996d8e2e986d83353028ae4b7e27ee4aba4
Muskdscer <D695312811@163.com>
2025-08-12 11:31:50 +0800

    贪心 合并区间 划分字母区间

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/greedy/Merge.java
@@ -0,0 +1,29 @@
+package com.example.algorithm2025.leetcode01.greedy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Merge {
+    public int[][] merge(int[][] intervals) {
+        List<int[]> res = new LinkedList<>();
+        //左边界排序
+        Arrays.sort(intervals, (x, y) -> Integer.compare(x[0], y[0]));
+        int start = intervals[0][0];
+        int rightmostRightBound = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] > rightmostRightBound) {
+                //加入区间 更新start
+                res.add(new int[]{start, rightmostRightBound});
+                start = intervals[i][0];
+                rightmostRightBound = intervals[i][1];
+            } else {
+                //更新最大右边界
+                rightmostRightBound = Math.max(rightmostRightBound, intervals[i][1]);
+            }
+        }
+        res.add(new int[]{start, rightmostRightBound});
+        return res.toArray(new int[res.size()][]);
+    }
+}
---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/greedy/PartitionLabels.java
@@ -0,0 +1,25 @@
+package com.example.algorithm2025.leetcode01.greedy;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class PartitionLabels {
+    public List<Integer> partitionLabels(String S) {
+        List<Integer> list = new LinkedList<>();
+        int[] edge = new int[26];
+        char[] chars = S.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            edge[chars[i] - 'a'] = i ;
+        }
+        int idx = 0;
+        int last = -1;
+        for (int i = 0; i < chars.length; i++) {
+            idx = Math.max(idx, edge[chars[i] - 'a']);
+            if (i == idx) {
+                list.add(i - last);
+                last = i;
+            }
+        }
+        return list;
+    }
+}
