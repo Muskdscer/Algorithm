dff0c611c394b09ebfd8b5cf739194c24c109097
Muskdscer <D695312811@163.com>
2025-08-25 08:01:38 +0800

    模板模式注释

--- algorithm/src/main/java/com/example/algorithm/designPattern/template/PureMilk.java
+++ algorithm/src/main/java/com/example/algorithm/designPattern/template/PureMilk.java
@@ -1,11 +1,13 @@
 package com.example.algorithm.designPattern.template;
 
 public class PureMilk extends SoyaMilk1{
+    //覆盖添加配料方法决定加什么配料
     @Override
     void addCondiments() {
         System.out.println("不加配料");
     }
 
+    //覆盖钩子方法决定是否要添加配料 return true or false
     @Override
     boolean customerWantCondiment() {
         return true;
