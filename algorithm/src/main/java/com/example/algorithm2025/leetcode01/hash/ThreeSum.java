ea94133d0b5e69dfdaba3fc292535f0e720a00cf
Muskdscer <D695312811@163.com>
2025-08-29 16:14:50 +0800

    hot100 三数之和

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/hash/ThreeSum.java
@@ -0,0 +1,42 @@
+package com.example.algorithm2025.leetcode01.hash;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                return result;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                if (sum > 0) {
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (right > left && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    while (right > left && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    //这种情况下sum=0 如果只有单独 right--或者left++不可能有sum=0 只有同时++和--才有可能
+                    right--;
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+}
