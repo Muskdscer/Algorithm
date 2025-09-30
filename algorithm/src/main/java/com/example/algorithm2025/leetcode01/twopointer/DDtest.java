7f8d8a79614ba1c80092f9fd451fc4418c45109e
Muskdscer <D695312811@163.com>
2025-09-30 17:07:50 +0800

    菜鸟网络算法题

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/twopointer/DDtest.java
@@ -0,0 +1,35 @@
+package com.example.algorithm2025.leetcode01.twopointer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DDtest {
+    int sum;
+    public int getNearSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int sum = 0;
+        int absSum =Integer.MAX_VALUE;
+        Map<Integer, Integer> res = new HashMap<>();
+        for (int i = 0; i < nums.length - 3; i++) {
+            left = i + 1;
+            int tempNum = nums[i] + nums[left] + nums[right];
+            sum += tempNum;
+            while (left < right) {
+                if (sum < target) {
+                    left++;
+                    absSum = Math.min(Math.abs(sum - target),absSum);
+                    res.put(absSum, sum);
+                }
+                if (sum > target) {
+                    right--;
+                    absSum = Math.min(Math.abs(sum - target),absSum);
+                }
+                if (sum == target) {
+                    return sum;
+                }
+            }
+        }
+        return
+    }
+}
