7884c8050818ad56ca620549ab2b71070c43c73a
DSC <D695312811@163.com>
2025-10-25 21:44:18 +0800

    两数之和

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/hash/TwoSum2.java
@@ -0,0 +1,19 @@
+package com.example.algorithm2025.leetcode01.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//两数之和
+public class TwoSum2 {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
