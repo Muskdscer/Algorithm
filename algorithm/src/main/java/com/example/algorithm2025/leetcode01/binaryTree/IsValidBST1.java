86e3fe274e28e49b99203d41710fc83ec0be34ee
DSC <D695312811@163.com>
2025-10-26 16:02:36 +0800

    验证二叉搜索树

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/IsValidBST1.java
@@ -0,0 +1,23 @@
+package com.example.algorithm2025.leetcode01.binaryTree;
+
+public class IsValidBST1 {
+    TreeNode max;
+    public boolean isValidBST(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        //左
+        boolean left = isValidBST(root.left);
+        if (!left) {
+            return false;
+        }
+        //中
+        if (max != null && max.val >= root.val) {
+            return false;
+        }
+        max = root;
+        //右
+        boolean right = isValidBST(root.right);
+        return right;
+    }
+}
