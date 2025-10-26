1a0e4489a68afc64d3343ad5f4523e9d6774f950
DSC <D695312811@163.com>
2025-10-26 12:35:27 +0800

    合并两个有序链表

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/linkedlist/MergeTwoLists2.java
@@ -0,0 +1,33 @@
+package com.example.algorithm2025.leetcode01.linkedlist;
+
+public class MergeTwoLists2 {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode curA = list1;
+        ListNode curB = list2;
+        ListNode head = new ListNode(0);
+        ListNode cur = head;
+        while (curA != null && curB != null) {
+            if (curA.val <= curB.val) {
+                cur.next = curA;
+                curA = curA.next;
+                cur  = cur.next;
+            } else  {
+                cur.next = curB;
+                curB = curB.next;
+                cur  = cur.next;
+            }
+        }
+
+        while (curA != null) {
+            cur.next = curA;
+            curA = curA.next;
+            cur  = cur.next;
+        }
+        while (curB != null) {
+            cur.next = curB;
+            curB = curB.next;
+            cur  = cur.next;
+        }
+        return head.next;
+    }
+}
