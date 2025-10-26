51394272ddf5a6c074646db3d264a056406ecad6
DSC <D695312811@163.com>
2025-10-26 16:42:02 +0800

    每日温度

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/stack/DailyTemperatures1.java
@@ -0,0 +1,27 @@
+package com.example.algorithm2025.leetcode01.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+//每日温度
+public class DailyTemperatures1 {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int lens = temperatures.length;
+        int[] res = new int[lens];
+
+        Deque<Integer> stack = new LinkedList<>();
+        stack.push(0);
+        for (int i = 1; i < lens; i++) {
+            if (temperatures[i] <= temperatures[stack.peek()]) {
+                stack.push(i);
+            } else {
+                while (!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                    res[stack.peek()] = i -stack.peek();
+                    stack.pop();
+                }
+                stack.push(i);
+            }
+        }
+        return res;
+    }
+}
