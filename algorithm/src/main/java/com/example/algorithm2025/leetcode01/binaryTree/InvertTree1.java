d36ca152000df65abadee63117dd991e44e6ca50
DSC <D695312811@163.com>
2025-10-26 12:58:05 +0800

    翻转二叉树

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/InvertTree1.java
@@ -0,0 +1,19 @@
+package com.example.algorithm2025.leetcode01.binaryTree;
+
+public class InvertTree1 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        swap(root, left, right);
+        return root;
+    }
+
+    private void swap(TreeNode root, TreeNode left, TreeNode right) {
+        TreeNode temp = left;
+        root.left = right;
+        root.right = temp;
+    }
+}
