b420863b9a1cfe865add7868673b02610f0fc81c
DSC <D695312811@163.com>
2025-10-26 16:00:18 +0800

    有序数组转换为二叉搜索树

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/SortedArrayToBST1.java
@@ -0,0 +1,17 @@
+package com.example.algorithm2025.leetcode01.binaryTree;
+
+public class SortedArrayToBST1 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return traversal(nums, 0, nums.length - 1);
+    }
+    private TreeNode traversal(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = traversal(nums, left, mid - 1);
+        root.right = traversal(nums, mid + 1, right);
+        return root;
+    }
+}
