20cd3390e73e2d1b9f3983d843e0ef89e4637974
DSC <D695312811@163.com>
2025-10-26 17:02:01 +0800

    前K个高频元素

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/heap/TopKFrequent.java
@@ -0,0 +1,23 @@
+package com.example.algorithm2025.leetcode01.heap;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+public class TopKFrequent {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num,map.getOrDefault(num, 0) + 1);
+        }
+        PriorityQueue<int[]> pq =  new PriorityQueue<>((pair1, pair2) -> pair2[1] - pair1[1]);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.add(new int[]{entry.getKey(), entry.getValue()});
+        }
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll()[0];
+        }
+        return ans;
+    }
+}
