584d115299af41611098ed91f1a8aba2f52fb348
DSC <D695312811@163.com>
2025-10-26 12:26:17 +0800

    回文链表

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/linkedlist/IsPalindrome1.java
@@ -0,0 +1,24 @@
+package com.example.algorithm2025.leetcode01.linkedlist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IsPalindrome1 {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+        int start = 0;
+        int end = list.size() - 1;
+        while (start < end) {
+            if (list.get(start) != list.get(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
