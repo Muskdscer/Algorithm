466d19597a1e1ca7d66136ab1e73ac9858f4c40a
DSC <D695312811@163.com>
2025-10-26 12:38:50 +0800

    合并K个升序链表

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/linkedlist/MergeKLists2.java
@@ -0,0 +1,35 @@
+package com.example.algorithm2025.leetcode01.linkedlist;
+
+//合并K个升序链表
+public class MergeKLists2 {
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode ans = null;
+        for (int i = 0; i < lists.length; i++) {
+            ans = merge(ans, lists[i]);
+        }
+        return ans;
+    }
+
+    private ListNode merge(ListNode l1, ListNode l2) {
+        if (l1 == null || l2 == null) {
+            return l1 == null ? l2 : l1;
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode curA = l1;
+        ListNode curB = l2;
+        ListNode cur = dummy;
+        while (curA != null && curB != null) {
+            if (curA.val <= curB.val) {
+                cur.next = curA;
+                curA = curA.next;
+            } else {
+                cur.next = curB;
+                curB = curB.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = curA == null ? curB : curA;
+        return dummy.next;
+    }
+}
