3a50d4e4e55582fdb870259a9e93d14115615770
DSC <D695312811@163.com>
2025-10-26 16:06:42 +0800

    二叉树右视图

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/binaryTree/RightSideView1.java
@@ -0,0 +1,33 @@
+package com.example.algorithm2025.leetcode01.binaryTree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class RightSideView1 {
+    List<Integer> res = new ArrayList<>();
+    Queue<TreeNode> que = new LinkedList<>();
+    public List<Integer> rightSideView(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        que.offer(root);
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = que.poll();
+                if (node.left != null) {
+                    que.offer(node.left);
+                }
+                if (node.right != null) {
+                    que.offer(node.right);
+                }
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+            }
+        }
+        return res;
+    }
+}
