819c33f765d89f4bcce1c1f40e0c8b3de764b716
DSC <D695312811@163.com>
2025-10-26 12:09:20 +0800

    相交链表

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/linkedlist/GetIntersectionNode1.java
@@ -0,0 +1,45 @@
+package com.example.algorithm2025.leetcode01.linkedlist;
+
+//相交链表
+public class GetIntersectionNode1 {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        int lenA = 0;
+        int lenB = 0;
+        ListNode curA = headA;
+        ListNode curB = headB;
+        while (curA != null) {
+            curA = curA.next;
+            lenA++;
+        }
+        while (curB != null) {
+            curB = curB.next;
+            lenB++;
+        }
+        curA = headA;
+        curB = headB;
+        if (lenB > lenA) {
+            ListNode temp = curA;
+            curA = curB;
+            curB= temp;
+            int lenTemp = lenA;
+            lenA = lenB;
+            lenB = lenTemp;
+        }
+
+        int gap = lenA - lenB;
+        while (gap-- > 0) {
+            curA = curA.next;
+        }
+        while (curA != null) {
+            if (curA== curB) {
+                return curA;
+            }
+            curA = curA.next;
+            curB = curB.next;
+        }
+        return null;
+    }
+}
