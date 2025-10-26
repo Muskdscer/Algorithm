59f02d8f96306445623c64abc5ae4cf68d1f48d6
DSC <D695312811@163.com>
2025-10-26 09:42:49 +0800

    和为K的子数组

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/subarray/SubarraySum2.java
@@ -0,0 +1,21 @@
+package com.example.algorithm2025.leetcode01.subarray;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SubarraySum2 {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int pre = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            pre += nums[i];
+            if (map.containsKey(pre - k)) {
+                count += map.get(pre - k);
+            }
+            map.put(pre, map.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+}
