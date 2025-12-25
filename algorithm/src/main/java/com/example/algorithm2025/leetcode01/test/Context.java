bb4224b398cd201400e337a2fc2997b558cf72cf
Muskdscer <D695312811@163.com>
2025-12-25 13:52:05 +0800

    状态模式

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/test/Context.java
@@ -0,0 +1,17 @@
+package com.example.algorithm2025.leetcode01.test;
+
+public class Context {
+    private State state;
+
+    public Context(){
+        state = null;
+    }
+
+    public void setState(State state){
+        this.state = state;
+    }
+
+    public State getState(){
+        return state;
+    }
+}
---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/test/State.java
@@ -0,0 +1,5 @@
+package com.example.algorithm2025.leetcode01.test;
+
+public interface State {
+    public void doAction(Context context);
+}
