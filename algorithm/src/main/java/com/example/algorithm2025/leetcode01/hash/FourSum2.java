c514e2bc81760909a5e7dd2824a51ceb4b6879f2
Muskdscer <D695312811@163.com>
2026-01-07 10:48:15 +0800

    四数之和

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/hash/FourSum2.java
@@ -0,0 +1,49 @@
+package com.example.algorithm2025.leetcode01.hash;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FourSum2 {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > target) {
+                return result;
+            }
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] >=0 && nums[i] + nums[j] > target) {
+                    return result;
+                }
+                if (j > i + 1 && nums[j - 1] == nums[j]) {
+                    continue;
+                }
+                int left = j + 1;
+                int right = nums.length - 1;
+                while (left < right) {
+                    int sum = nums[i] + nums[j] + nums[left] + nums[right];
+                    if (sum < 0) {
+                        left++;
+                    } else if (sum > 0) {
+                        right--;
+                    } else {
+                        result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+                        while (left < right && nums[right] == nums[right - 1]) {
+                            right--;
+                        }
+                        while (left < right && nums[left] == nums[left + 1]) {
+                            left++;
+                        }
+                        right--;
+                        left++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
--- algorithm/src/main/java/com/example/algorithm2025/leetcode01/hash/ThreeSum1.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/hash/ThreeSum1.java
@@ -10,7 +10,7 @@ public class ThreeSum1 {
         Arrays.sort(nums);
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] > 0) {
-                continue;
+                return result;
             }
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
