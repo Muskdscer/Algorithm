e0c7b8feb522ef41af2fc2e35bf36d8c5cf23b22
Muskdscer <D695312811@163.com>
2026-01-07 09:46:53 +0800

    两数之和

---
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/hash/TwoSum3.java
@@ -0,0 +1,17 @@
+package com.example.algorithm2025.leetcode01.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum3 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
