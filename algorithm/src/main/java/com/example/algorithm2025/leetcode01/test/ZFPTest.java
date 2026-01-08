05ca9c534d6261883e1de6d5f593fad1382f33b5
Muskdscer <D695312811@163.com>
2026-01-08 08:16:07 +0800

    智平方面试题错误修改

--- algorithm/src/main/java/com/example/algorithm2025/leetcode01/test/ZFPTest.java
+++ algorithm/src/main/java/com/example/algorithm2025/leetcode01/test/ZFPTest.java
@@ -3,15 +3,13 @@ package com.example.algorithm2025.leetcode01.test;
 //智平方面试题
 public class ZFPTest {
     public LinkedListNode findNode(LinkedListNode startNode) {
-        LinkedListNode dummy = new LinkedListNode();
-        dummy.next = startNode;
-        LinkedListNode fast = dummy;
-        LinkedListNode slow  = dummy;
+        LinkedListNode fast = startNode;
+        LinkedListNode slow  = startNode;
         while (fast.next.next != null && slow.next != null) {
             fast = fast.next.next;
             slow = slow.next;
-            if (fast.val == slow.val) {
-                LinkedListNode newSlow = dummy;
+            if (fast == slow) {
+                LinkedListNode newSlow = startNode;
                 LinkedListNode newFast = fast;
                 while (newSlow.next != null && newFast.next != null) {
                     newFast = newFast.next;
